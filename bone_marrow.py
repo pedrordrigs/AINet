cff4ddde669fcaeeb39777288b1cd308959dd627
pedrorodrigues <pedro.correa@creditas.com>
2022-09-15 22:01:47 -0300

    random chromossome for tsp path

--- bone_marrow.py
+++ bone_marrow.py
@@ -2,13 +2,17 @@ import random
 
 # initialize a 8 genes long cell by randomness
 
-def create_cell():
-    gene_library = ['1A','1B','1C','1D','2A','2B','2C','2D']
+def create_cell(cell_len, gene_library):
+    cell_string = ''
+    # gene_library = [['7 1 8 ', '1 6 9 ', '9 1 7 ', '3 4 5 '],['8 2 5 ', '1 9 7 ', '2 4 3 ', '1 6 2 '],['1 8 2 ', '6 1 9 ', '7 1 9 ', '6 1 2 ']]
     cell = []
-    for i in range(8):
-        cell.append(gene_library[random.randint(0, 7)])
-    print(cell)
-    return cell
+    for i in range(cell_len):
+        lib =  random.randint(0, len(gene_library)-1)
+        gene = random.randint(0, len(gene_library[0])-1)
+        cell.append(gene_library[lib][gene])
+    for i in range(len(cell)):
+        cell_string = cell_string + str(cell[i])
+    return cell_string
 
 def affinity(cell1, cell2):
     affinity = 0
@@ -17,3 +21,10 @@ def affinity(cell1, cell2):
             affinity += 1
     return affinity
 
+def fix(cell):
+    cell = cell.replace(" ", "")
+    print(cell)
+
+cell = create_cell(3)
+print(cell)
+fix(cell)
---
+++ tsp_solver.py
@@ -0,0 +1,91 @@
+import random
+from opentsp.objects import Generator
+
+def tsp_generator(len):
+    gen = Generator()
+    instance = gen.new_instance(len, source='seed', seed=1234)
+    instance.view(nodes=True, edges=True)
+    return instance
+
+def gen_libraries(instance, num_libraries, library_size):
+    x = instance.x_values
+    y = instance.y_values
+    gene_libraries = []
+    for i in range(num_libraries):
+        coord = []
+        for i in range(len(x)):
+            coord.append([x[i],y[i]])
+
+        gene_lib = []
+
+        for j in range(library_size):
+            l = 0
+            n = 0
+            while(l == n):
+                l = random.randint(0, len(coord)-1)
+                n = random.randint(0, len(coord)-1)
+            gene_lib.append(coord[l] + coord[n])
+
+        gene_libraries.append(gene_lib)
+    return gene_libraries
+
+def gen_chromossome(gene_libraries,num_libraries):
+    selected_genes = []
+    for i in range(num_libraries):
+        selected_genes.append(gene_libraries[i][random.randint(0, len(gene_libraries[i])-1)])
+    return selected_genes
+
+def repair_chromossome(chromossome, instance, num_libraries):
+    repaired = []
+    x = instance.x_values
+    y = instance.y_values
+    for i in range(num_libraries):
+        coord = []
+        for i in range(len(x)):
+            coord.append([x[i],y[i]])
+
+    for i in range(len(chromossome)):
+        for j in range(0, len(chromossome[i]), 2):
+            repaired.append([chromossome[i][j], chromossome[i][j+1]])
+
+    print(repaired)
+    replace = [[]]
+    for i in range (len(repaired)):
+        for j in range (len(repaired)):
+            if(repaired[i] == repaired[j] and i != j):
+                check = 1
+                m = 0
+                while(check != 0):
+                    check = 0
+                    replace[0] = (coord[m])
+                    for n in range(len(repaired)):
+                        if(repaired[n] == replace[0]):
+                            check += 1
+                    m += 1
+                repaired[j] = replace[0]
+
+    print(repaired)
+    return(repaired)
+
+
+
+# def fitness(path, instance):
+
+
+
+def tsp_solver():
+    tsp_size = 8
+    num_libraries = tsp_size/2
+    library_size = num_libraries/2
+
+    num_libraries = int(num_libraries)
+    library_size = int(library_size)
+
+    tsp = tsp_generator(tsp_size)
+    gene_libraries = gen_libraries(tsp, num_libraries, library_size)
+    chromossome = gen_chromossome(gene_libraries, num_libraries)
+    path = repair_chromossome(chromossome, tsp, num_libraries)
+
+    return(path)
+
+tsp_solver()
